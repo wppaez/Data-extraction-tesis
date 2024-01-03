7a4cac0462de3e8ec5ecf4f9282508cc2c0bb6d8
wppaez <wppaez@uninorte.edu.co>
2024-01-03 09:20:22 -0500

    Primer commit

---
+++ igdata.py
@@ -0,0 +1,55 @@
+from unicodedata import name
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import urllib.request
+import requests
+import ssl
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+# Variables de entorno
+INSTAGRAM_USER = os.getenv("data.user_wppb")
+INSTAGRAM_PASSWORD = os.getenv("Cliford99")
+
+
+PATH = "/Users/oktanait/Downloads/chromedriver"
+driver = webdriver.Chrome(PATH)
+
+driver.get("https://www.instagram.com/")
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+# login
+time.sleep(4)
+username = driver.find_element("css selector", "input[name='username']")
+password = driver.find_element("css selector", "input[name='password']")
+username.clear()
+password.clear()
+username.send_keys(INSTAGRAM_USER)
+password.send_keys(INSTAGRAM_PASSWORD)
+login = driver.find_element("css selector", "button[type='submit']").click()
+
+time.sleep(4)
+nothow = driver.find_element(
+    "xpath", "//button[contains(text(), 'Not Now')]").click()
+# turn on notif
+time.sleep(4)
+nothow2 = driver.find_element(
+    "xpath", "//button[contains(text(), 'Not Now')]").click()
+
+
+# searchbox
+time.sleep(5)
+searchbox1 = driver.find_element(
+    "css selector", "svg[aria-label='Explore']").click()
+time.sleep(5)
+searchbox = driver.find_element("css selector", "input[placeholder='Search']")
+searchbox.clear()
+searchbox.send_keys("rappicolombia")
+time.sleep(4)
+searchbox.send_keys(Keys.ENTER)
+time.sleep(4)
+searchbox.send_keys(Keys.ENTER)
+time.sleep(4)
